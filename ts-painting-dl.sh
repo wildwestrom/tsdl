a4ed0ba576074a2118a72798e0490412a83fa80e
Christian Westrom <c.westrom@westrom.xyz>
2020-07-21 01:31:36 -0400

    Multiple changes to the script. Added temporary file creation. Added cleanup on SYSINT (ctrl+c) signal.

--- ts-painting-dl.sh
+++ ts-painting-dl.sh
@@ -16,29 +16,52 @@ printf "\rProgress : [${_fill// /#}${_empty// /-}] ${_progress}%%"
 }
 
 #Check for dependencies.
-printf "Checking dependencies... "
-for name in lynx curl wget awk sed
-do
-  [[ $(command -v $name 2>&1) ]] || { printf "\nDependency Missing: $name needs to be installed. $name'";deps=1; }
-done
-[[ $deps -ne 1 ]] && printf "OK" || { printf "\nInstall the above and rerun this script\n";exit 1; }
+function DependencyCheck() {
+    echo "Checking dependencies... "
+    for prog in lynx curl wget awk sed
+    do
+      [[ $(command -v $prog 2>&1) ]] || { printf "\nDependency Missing: $prog needs to be installed. $prog'";deps=1; }
+    done
+    [[ $deps -ne 1 ]] && printf "OK\n" || { printf "\nInstall the above and rerun this script\n";exit 1; }
+}
 
-rm -f cookie.file
-rm -f img-pages.txt
-rm -f HQ-img-links.txt
-rm -f lynx.cfg
+DependencyCheck
 
-#Prompts user for login to TS Premium
-printf "SET_COOKIES:TRUE\nACCEPT_ALL_COOKIES:TRUE\nPERSISTENT_COOKIES:TRUE\nCOOKIE_FILE:cookie.file" > lynx.cfg
-printf "Please log in to Teal Swan Premium.\n"
-lynx -cfg=lynx.cfg https://tealswan.com/login
-printf "\n"
+tempdir=$(mktemp -d)
+
+function cleanup {
+    rm -f $tempdir/cookies
+    rm -f $tempdir/img-pages.txt
+    rm -f $tempdir/HQ-img-links.txt
+    rm -f $tempdir/lynx.cfg
+}
+
+function trap_ctrlc ()
+{
+    cleanup 
+    echo "\n SIGINT detected; Stopping program."
+    exit 2
+}
+ 
+trap "trap_ctrlc" 2
+
+##Prompts user for login to TS Premium
+#read -p    "Input Username/Email " USER
+#read -s -p "Input Password "       PASS
+#echo '\n'
+
+printf "SET_COOKIES:TRUE\nACCEPT_ALL_COOKIES:TRUE\nPERSISTENT_COOKIES:TRUE\nCOOKIE_FILE:$tempdir/cookies" > $tempdir/lynx.cfg
+lynx -cfg=$tempdir/lynx.cfg -post_data "auth=$USER&password=$PASS"--- https://tealswan.com/login
+
+#curl -s -c $tempdir/cookies -d "auth=$USER&password=$PASS" -D $tempdir/headerdump -L https://tealswan.com/login/ | less
+
+echo '\n'
 
 # Gets links to the pages with the image download link.
-PAGES=`curl -s -b cookie.file https://tealswan.com/paintings/ | grep -m 1 "Page 1 of" | grep -Eo '[0-9] +' | awk 'NR==2'`
+PAGES=`curl -s -b $tempdir/cookies https://tealswan.com/paintings/ | grep -m 1 "Page 1 of" | grep -Eo '[0-9] +' | awk 'NR==2'`
 
 for (( i = 1; i <= $PAGES; i++ )); do
-  curl -s -b cookie.file https://tealswan.com/paintings/page/$i | grep "background-image" | awk 'BEGIN{
+  curl -s -b $tempdir/cookie.file https://tealswan.com/paintings/page/$i | grep "background-image" | awk 'BEGIN{
 RS="</a>"
 IGNORECASE=1
 }
@@ -50,11 +73,11 @@ IGNORECASE=1
       print $(o)
     }
   }
-}' >> img-pages.txt
+}' >> $tempdir/img-pages.txt
   ProgressBar ${i} ${PAGES}
 done
 
-TOTAL_IMAGES=`wc -l < img-pages.txt`
+TOTAL_IMAGES=`wc -l < $tempdir/img-pages.txt`
 
 printf "\n$TOTAL_IMAGES paintings to be downloaded.\n"
 # Gets the links to the desired images and names of images.
@@ -62,7 +85,7 @@ printf "\nDownloading links to images.\n"
 COUNT=1
 while read LINE
 do
-  curl -s -b cookie.file $LINE > PAGE
+  curl -s -b $tempdir/cookies $LINE > $tempdir/PAGE
 
   LINK=$(grep -m 1 "download href=" < PAGE | awk 'BEGIN{
   RS="</a>"
@@ -78,15 +101,15 @@ do
     }
   }')
 
-   NAME=$(grep "<title>" < PAGE | sed -E 's/[[:space:]]+<title>//; s/ - Paintings.*>//; s/ /_/g')
+   NAME=$(grep "<title>" < PAGE \
+   | sed -E 's/[[:space:]]+<title>//; s/ - Paintings.*>//; s/ /_/g')
 
-   printf "$LINK;$NAME\n" >> HQ-img-links.txt
+   printf "$LINK;$NAME\n" >> $tempdir/HQ-img-links.txt
 
    ProgressBar ${COUNT} ${TOTAL_IMAGES}
    COUNT=`expr $COUNT + 1`
 
-done < img-pages.txt
-rm -f PAGE
+done < $tempdir/img-pages.txt
 
 # Downloading the actual images.
 printf "\nDownloading images.\n"
@@ -98,9 +121,17 @@ do
   ProgressBar ${COUNT} ${TOTAL_IMAGES}
   COUNT=`expr $COUNT + 1`
 
-done < HQ-img-links.txt
+done < $tempdir/HQ-img-links.txt
 
-rm -f cookie.file
-rm -f HQ-img-links.txt
-rm -f img-pages.txt
-rm -f lynx.cfg
+cleanup
+
+
+
+
+
+
+
+
+
+
+
